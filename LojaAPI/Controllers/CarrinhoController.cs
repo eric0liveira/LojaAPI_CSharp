6b2a7ff0bbea0fe745e8b12cf90da3a443d68130
Eric Oliveira <eric.erikito@gmail.com>
2018-02-07 12:42:41 -0200

    Serviços Web REST e Addressability

---
+++ LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,16 @@
+﻿using LojaAPI.DAO;
+using LojaAPI.Models;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public string Get(int id)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(id);
+            return carrinho.ToXml();
+        }
+    }
+}
