5d5a2c2cdc781f58b9e201b32af74432870cedce
Eric Oliveira <eric.erikito@gmail.com>
2018-02-08 10:19:55 -0200

    Status code e a interface uniforme

--- LojaAPI/Controllers/CarrinhoController.cs
+++ LojaAPI/Controllers/CarrinhoController.cs
@@ -1,23 +1,41 @@
 ﻿using LojaAPI.DAO;
 using LojaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace LojaAPI.Controllers
 {
     public class CarrinhoController : ApiController
     {
-        public Carrinho Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            CarrinhoDAO dao = new CarrinhoDAO();
-            Carrinho carrinho = dao.Busca(id);
-            return carrinho;
+            try
+            {
+                CarrinhoDAO dao = new CarrinhoDAO();
+                Carrinho carrinho = dao.Busca(id);
+                return Request.CreateResponse(HttpStatusCode.OK, carrinho);
+            }
+            catch (KeyNotFoundException)
+            {
+                string mensagem = string.Format("O carrinho {0} não foi encontrado", id);
+                HttpError error = new HttpError(mensagem);
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);
+            }            
         }
 
-        public string Post([FromBody] Carrinho carrinho)
+        public HttpResponseMessage Post([FromBody] Carrinho carrinho)
         {
             CarrinhoDAO dao = new CarrinhoDAO();
             dao.Adiciona(carrinho);
-            return "suceso";
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+            string location = Url.Link("DefaultApi", new { controller = "carrinho", id = carrinho.Id });
+            response.Headers.Location = new Uri(location);
+
+            return response;
         }
     }
 }
